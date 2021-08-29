14e8658217af531150a9f5e99760c3a5ae8d393b
Kirill Kaiumov <kirill.kaiumov@uula.com>
2021-08-29 14:53:06 +0300

    Add vertical lines

--- code.rb
+++ code.rb
@@ -10,11 +10,33 @@ require 'rqrcode_core'
 require 'pry'
 require 'erb'
 
-qr = RQRCodeCore::QRCode.new("https://www.uula.com")
+qr = RQRCodeCore::QRCode.new("https://www.uulastaging.com/backend/courses/95/quizzes/207/questions/398/edit")
+qr_size = qr.modules.size
 
-qr_html = qr.modules.map do |row|
-  row_html = row.map do |col|
-    "<div class=\"col #{col ? 'filled' : ''}\"></div>"
+vertical_lines_info = Array.new(qr_size).map { Array.new(qr_size) }
+
+qr.modules.each.with_index do |row, row_index|
+  row.each.with_index do |col, col_index|
+    next unless col
+
+    is_checked_above = row_index > 0 && qr.modules[row_index - 1][col_index]
+    is_checked_below = row_index < qr_size - 1 && qr.modules[row_index + 1][col_index]
+
+    vertical_lines_info[row_index][col_index] = if !is_checked_above && !is_checked_below
+      :dot
+    elsif !is_checked_above
+      :line_head
+    elsif !is_checked_below
+      :line_tail
+    else
+      nil
+    end
+  end
+end
+
+qr_html = qr.modules.map.with_index do |row, row_index|
+  row_html = row.map.with_index do |col, col_index|
+    "<div class=\"col #{col ? 'filled' : ''} #{vertical_lines_info[row_index][col_index]}\"></div>"
   end.join
 
   "<div class=\"row\">#{row_html}</div>"
