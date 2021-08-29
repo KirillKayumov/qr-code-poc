8b54412044e7eca3dbe2c5d4187b9da4a6b475e1
Kirill Kaiumov <kirill.kaiumov@uula.com>
2021-08-29 22:59:37 +0300

    Correctly round modules around logo

--- Readme.md
+++ Readme.md
@@ -11,6 +11,6 @@ open qr.html
 
 # Save image
 ```
-"/Applications/Google Chrome.app/Contents/MacOS/Google Chrome" --headless --screenshot --window-size=418,522 qr.html
+"/Applications/Google Chrome.app/Contents/MacOS/Google Chrome" --headless --screenshot --window-size=418,522 --default-background-color=00000000 qr.html
 open screenshot.png
 ```
--- code.rb
+++ code.rb
@@ -1,10 +1,3 @@
-# require "bundler/inline"
-
-# gemfile do
-#   source "https://rubygems.org"
-#   gem "rqrcode_core"
-# end
-
 require 'bundler/setup'
 require 'rqrcode_core'
 require 'pry'
@@ -14,14 +7,25 @@ require 'erb'
 qr = RQRCodeCore::QRCode.new("https://www.uula.com")
 qr_size = qr.modules.size
 
+qr_modules = qr.modules.map.with_index do |row, row_index|
+  row.map.with_index do |col, col_index|
+    next false if (0..6).include?(row_index) && (0..6).include?(col_index)
+    next false if (0..6).include?(row_index) && ((qr_size - 7)..(qr_size - 1)).include?(col_index)
+    next false if ((qr_size - 7)..(qr_size - 1)).include?(row_index) && (0..6).include?(col_index)
+    next false if (((qr_size / 2) - 4)..((qr_size / 2) + 4)).include?(row_index) && (((qr_size / 2) - 4)..((qr_size / 2) + 4)).include?(col_index)
+
+    col
+  end
+end
+
 vertical_lines_info = Array.new(qr_size).map { Array.new(qr_size) }
 
-qr.modules.each.with_index do |row, row_index|
+qr_modules.each.with_index do |row, row_index|
   row.each.with_index do |col, col_index|
     next unless col
 
-    is_checked_above = row_index > 0 && qr.modules[row_index - 1][col_index]
-    is_checked_below = row_index < qr_size - 1 && qr.modules[row_index + 1][col_index]
+    is_checked_above = row_index > 0 && qr_modules[row_index - 1][col_index]
+    is_checked_below = row_index < qr_size - 1 && qr_modules[row_index + 1][col_index]
 
     vertical_lines_info[row_index][col_index] = if !is_checked_above && !is_checked_below
       :dot
@@ -29,20 +33,12 @@ qr.modules.each.with_index do |row, row_index|
       :line_head
     elsif !is_checked_below
       :line_tail
-    else
-      nil
     end
   end
 end
 
-qr_html = qr.modules.map.with_index do |row, row_index|
+qr_html = qr_modules.map.with_index do |row, row_index|
   row_html = row.map.with_index do |col, col_index|
-    next "<div class=\"col\"></div>" if (0..6).include?(row_index) && (0..6).include?(col_index)
-    next "<div class=\"col\"></div>" if (0..6).include?(row_index) && ((qr_size - 7)..(qr_size - 1)).include?(col_index)
-    next "<div class=\"col\"></div>" if ((qr_size - 7)..(qr_size - 1)).include?(row_index) && (0..6).include?(col_index)
-
-    next "<div class=\"col\"></div>" if (((qr_size / 2) - 4)..((qr_size / 2) + 4)).include?(row_index) && (((qr_size / 2) - 4)..((qr_size / 2) + 4)).include?(col_index)
-
     "<div class=\"col #{col ? 'filled' : ''} #{vertical_lines_info[row_index][col_index]}\"></div>"
   end.join
 
